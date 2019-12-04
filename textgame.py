dc5da41007a18e2894517906d8ae917b16c5ce10
kevinrull1 <55434499+kevinrull1@users.noreply.github.com>
2019-12-04 13:41:59 +0200

    Add files via upload

---
+++ textgame.py
@@ -0,0 +1,29 @@
+import time
+
+print('Mis on teie nimi:')
+x = input()
+print('Tere, ' + x)
+
+
+
+
+
+print("Sa üks päev üles ja mõtled, et tahaks minna reisile.\n\
+Kui sa tööle jõuad siis sa küsid oma ülemuse käest kas sa saaks võtta nädala vabaks.\n\
+Sa jõudad töölt koju ja hakkad mõtlema kuhu minna. Umbes tunni aja pärast on sul asukoht välja valitud.\n\
+Valisid asukohaks Hawaii. Hakkad asju juba pakkima, sest lennuk läheb juba mõne tunni pärast.\n\
+") 
+
+time.sleep(1.5)
+print("Sa oled lennujaamas ja valmis lennuki peale minema. Peale poolt tundi oled lõpuks lennuki peal.\n\
+Lend on pikk. Sa oled väsinud ja lähed magama.")
+
+
+time.sleep(3)
+print("6 tundi hiljem...\n")
+
+
+time.sleep(2.5)
+print("Järsku sa kuuled suurt pauku. Sa vaatad ringi segaduses, ja vaatad aknast välja. Üks mootor on katki läinud.\n\
+Sa tunned kuidas lennuk hakkab allapoole liikuma ja sa hakkad paanitsema. Istud oma tooli tagasi ja loodad, et sa surma ei saa.\n\
+Sa näed aknast kuidas lennuk liigub ja liigub allapoole mere poole.")
